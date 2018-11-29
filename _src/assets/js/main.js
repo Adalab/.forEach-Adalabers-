42d713115c277cbe05401dcc0b7775cb6bff3fff
Beatriz Garcia <beatrizgarcia.jim@gmail.com>
2018-11-29 20:41:03 +0100

    input name js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,10 +6,17 @@ const inputName = document.querySelector("#name");
 const nameCard = document.querySelector(".userInfo__name"); 
 
 function handlerInput (){
+   
     let dataInput = inputName.value;
-    nameCard.innerHTML = dataInput;
+    if(inputName.value===""){
+        nameCard.innerHTML="Nombre Apellido";
+    }
+    else{
+        nameCard.innerHTML = dataInput;
+    };
+    
 };
 
-inputName.addEventListener("keypress",handlerInput); //when you write your name the Card Change
+inputName.addEventListener("keyup",handlerInput); //when you write your name the Card Change
 inputName.addEventListener("blur",handlerInput);// when you change the focus your name change (applicable after cutting and pasting)
-
+inputName.addEventListener("change",handlerInput);
