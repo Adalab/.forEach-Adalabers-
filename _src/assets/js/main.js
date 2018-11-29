7aa8b85a7e8181538ea4d7a00fbc4e7cfee99337
Beatriz Garcia <beatrizgarcia.jim@gmail.com>
2018-11-29 08:53:58 +0100

    Add JS input Name change

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('>> Ready :)');
+
+// JS for hidden (I think this page would be better in partial hidden but I do not know how to import it )
+const inputName = document.querySelector("#name");
+const nameCard = document.querySelector(".userInfo__name"); 
+
+function handlerInput (){
+    let dataInput = inputName.value;
+    nameCard.innerHTML = dataInput;
+};
+
+inputName.addEventListener("keypress",handlerInput); //when you write your name the Card Change
+inputName.addEventListener("blur",handlerInput);// when you change the focus your name change (applicable after cutting and pasting)
+
