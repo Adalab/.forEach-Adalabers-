c7584ed537bdaef68aa5b5175de1dbc70c1e0fa5
arranzizquierdo <laura.arranz88@gmail.com>
2018-11-30 20:18:03 +0100

    Form interactive job

--- _src/assets/js/partials/inputsFill.js
+++ _src/assets/js/partials/inputsFill.js
@@ -1,10 +1,12 @@
 'use strict';
 
 // JS for hidden (I think this page would be better in partial hidden but I do not know how to import it )
+
+//NAME
 const inputName = document.querySelector("#name");
 const nameCard = document.querySelector(".userInfo__name"); 
 
-function handlerInput (){
+function handlerInputName (){
    
     let dataInput = inputName.value;
     if(inputName.value===""){
@@ -15,6 +17,26 @@ function handlerInput (){
     };
 };
 
-inputName.addEventListener("keyup",handlerInput); //when you write your name the Card Change
-inputName.addEventListener("blur",handlerInput);// when you change the focus your name change (applicable after cutting and pasting)
-inputName.addEventListener("change",handlerInput);
+inputName.addEventListener("keyup",handlerInputName); //when you write your name the Card Change
+inputName.addEventListener("blur",handlerInputName);// when you change the focus your name change (applicable after cutting and pasting)
+inputName.addEventListener("change",handlerInputName);
+inputName.addEventListener("keydown",handlerInputName);
+
+//JOB
+const inputJob = document.querySelector("#job");
+const jobCard = document.querySelector(".userInfo__job"); 
+
+function handlerInputJob (){
+   
+    let dataInput = inputJob.value;
+    if(inputJob.value===""){
+        jobCard.innerHTML="Front-end developer";
+    } else{
+        jobCard.innerHTML = dataInput;
+    };
+};
+
+inputJob.addEventListener("keyup",handlerInputJob); //when you write your job the Card Change
+inputJob.addEventListener("blur",handlerInputJob);// when you change the focus your job change (applicable after cutting and pasting)
+inputJob.addEventListener("change",handlerInputJob);
+inputJob.addEventListener("keydown",handlerInputJob);
