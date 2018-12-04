fe07a04182b4502af92b003a21e4b98a86b6aed9
desigc <desi.gc93@gmil.com>
2018-12-04 20:46:29 +0100

    add one skill in the cardPreview

---
+++ _src/assets/js/partials/skills.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const checkboxElements = document.querySelectorAll('.checkbox-input');
+const skillsElement = document.querySelector('.list__skills');
+
+function handlerPrintSkills(event){
+    const clickedInput = event.currentTarget;
+    console.log(clickedInput);
+    console.dir(clickedInput);
+    reviewList();
+}
+
+for (let i=0; i< checkboxElements.length; i++){
+   
+   checkboxElements[i].addEventListener('click', handlerPrintSkills);
+   console.log(checkboxElements[i]);
+  
+}
+
+function reviewList (){
+    for (let i=0; i< checkboxElements.length; i++){
+       
+        if(checkboxElements[i].checked === true){
+        
+            skillsElement.innerHTML = `<li class="list__item-skills">Html</li>`;
+
+        }
+    }
+}
+
+
+
+/* <li class="list__item-skills">Html</li>
+                <li class="list__item-skills">css</li>
+                <li class="list__item-skills">gulp</li>
+                <li class="list__item-skills">git</li>
+                <li class="list__item-skills">Sass</li>
+                <li class="list__item-skills">JavaScript</li>
+                <li class="list__item-skills">AJAX</li>
+                <li class="list__item-skills">React</li>  */
