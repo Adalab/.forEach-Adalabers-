9c339e462e726da439782d6287f55569fed5d9ca
minamrp <ramosp.marina@gmail.com>
2018-12-09 12:59:31 +0100

    order code reset

--- _src/assets/js/partials/reset.js
+++ _src/assets/js/partials/reset.js
@@ -1,16 +1,21 @@
 const resetFormC = document.querySelector("#button__reset");
+
 const inputPhoneReset = document.querySelector("#phone");
 const inputLinkedinReset = document.querySelector("#linkedin");
 const inputGithubReset = document.querySelector("#github");
 const inputEmailReset = document.querySelector("#email");
 const inputJobReset = document.querySelector("#job");
 const inputNameReset = document.querySelector("#name");
+
 const nameCardReset = document.querySelector("#userInfo__name"); 
 const jobCardReset = document.querySelector("#userInfo__job"); 
 const emailCardReset = document.querySelector("#email"); 
 const GithubCardReset = document.querySelector("#github"); 
 const LinkedinCardReset = document.querySelector("#linkedin");
 const phoneCardReset = document.querySelector("#mobile"); 
+
+const profileImageR = document.querySelector('.image-preview__file');
+const profileImageSmallR = document.querySelector('.image-preview-small');
 
 //RESET FORM
 
@@ -29,7 +34,7 @@ function resetElementsForm (){
 };
 resetFormC.addEventListener("click",resetElementsForm);
 
-//RESET CARD PREVIEW
+//RESET FORM CARD PREVIEW
 
 function resetElementsCard (){
     const empty = "";
@@ -54,15 +59,36 @@ resetFormC.addEventListener("click",resetElementsCard);
 
 //RESET IMAGE CARD PREVIEW
 
-const uploadBtnR = document.querySelector('.image-link');
-const fileFieldR = document.querySelector('#img-selector');
-const profileImageR = document.querySelector('.image-preview__file');
-const profileImageSmallR = document.querySelector('.image-preview-small');
-
 function deleteImageR() {
     profileImageR.style.backgroundImage = `url(assets/images/silueta.jpg)`;
     profileImageSmallR.style.backgroundImage = "";
-}
-
+};
 resetFormC.addEventListener("click", deleteImageR);
 
+//RESET STYLE CARD
+
+const boxCardGeneral = document.querySelector("#box");
+const userInfoGeneral = document.querySelector("#userInfo");
+const fontUbuntu =document.querySelector("#font-Ubuntu");
+const fontComicSans =document.querySelector("#font-ComicSans");
+const fontMont =document.querySelector("#font-Montserrat");
+const fontIndie =document.querySelector("#font-Indie");
+const fontBaskerville =document.querySelector("#font-Baskerville");
+
+function resetStylesCard(){
+    let checkFormFont = [
+        fontUbuntu,
+        fontComicSans,
+        fontMont,
+        fontIndie,
+        fontBaskerville,
+    ];
+    for (let i = 0; i <checkFormFont.length; i++) {
+        if (checkFormFont[i].checked){
+            checkFormFont[i].checked = false; 
+        }
+    };
+};
+resetFormC.addEventListener("click", resetStylesCard);
+
+//RESET STYLE CARD PREVIEW
