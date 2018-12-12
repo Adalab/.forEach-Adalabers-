308c74261b4824251daa341d888d9b03ae3ed44a
rdfornieles <rdfornieles@gmail.com>
2018-12-12 15:27:15 +0100

    send back-end

--- _src/assets/js/partials/submit.js
+++ _src/assets/js/partials/submit.js
@@ -2,9 +2,11 @@ const submitEl = document.querySelector('.button-create');
 function handlerSubmit() {
     fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/', {
         method: 'POST',
-        body: JSON.stringify(dataBack),
-    })
+        body: JSON.stringify(dataBack)
+    }).then((responLink) => responLink.json())
+    .then((link) => console.log(link))
+    .catch((error) => console.log(error))
 }
 
 
-submitEl.addEventListener('click' handlerSubmit);
+submitEl.addEventListener('click', handlerSubmit);
