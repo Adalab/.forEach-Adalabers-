d6d4aec85aa1bb30a51e99ec4b285f47f7723aa0
Beatriz Garcia <beatrizgarcia.jim@gmail.com>
2018-12-14 11:40:38 +0100

    re-json

--- _src/assets/js/partials/json.js
+++ _src/assets/js/partials/json.js
@@ -1,27 +1,5 @@
-'use strict';
-function createCardData (){
 
-  const boxEl = document.querySelector("#box");
-  let colorValue = boxEl.value;
-  const userInfoEl = document.querySelector("#userInfo");
-  let fontValue = userInfoEl.value;
-  const userNameEl = document.querySelector(".userInfo__name");
-  let nameDataValue = userNameEl.value;
-  const userJobEl = document.querySelector(".userInfo__job");
-  let jobDataValue = userJobEl.value;
-  const userTelEl = document.querySelector(".mobile");
-  let telDataValue = userTelEl.href;
-  const userEmailEl = document.querySelector(".email");
-  let emailDataValue = userEmailEl.href;
-  const userLinkedinEl = document.querySelector(".linkedin");
-  let linkedinDataValue = userLinkedinEl.href;
-  const userGithubEl = document.querySelector(".github");
-  let githubDataValue = userGithubEl.href;
-  const userImgEl = document.querySelector(".image-preview__file");
-  let imgDataValue = userImgEl.style.backgroundImage;
-
-
-  let data = {
+  let dataBack = {
     palette: colorValue,
     typography: fontValue,
     name: nameDataValue,
@@ -33,5 +11,4 @@ function createCardData (){
     photo: imgDataValue,
     skills: ["HTML", "Sass", "JavaScript"]
   }
-}
 
