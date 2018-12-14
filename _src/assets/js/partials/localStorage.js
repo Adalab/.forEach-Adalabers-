34b137ce58a5cba9b61e455b8b57ec7f29fe4d22
Beatriz Garcia <beatrizgarcia.jim@gmail.com>
2018-12-14 19:14:36 +0100

    merge json

---
+++ _src/assets/js/partials/localStorage.js
@@ -0,0 +1,70 @@
+let saveData = JSON.parse(localStorage.getItem("dataBackLocal")); //saveData es mi cadena ahora objeto
+
+function printLocalStorage() {
+
+  if (saveData) {
+    dataBack = saveData
+  };
+
+  let nameCard = document.querySelector(".userInfo__name"); //name
+  const inputName = document.querySelector("#name");
+  let nameData = saveData.name;
+  inputName.value = nameData;
+  nameCard.innerHTML = nameData;
+
+  let jobCard = document.querySelector(".userInfo__job"); //job
+  const inputJob = document.querySelector("#job");
+  let jobData = saveData.job;
+  inputJob.value = jobData;
+  jobCard.innerHTML = jobData;
+
+  let emailCard = document.querySelector(".email"); //mail
+  const inputEmail = document.querySelector("#email");
+  let emailData = saveData.email;
+  inputEmail.value = emailData;
+  emailCard.href = `mailto:${emailData}`;
+
+  let phoneCard = document.querySelector(".mobile"); //phone
+  const inputPhone = document.querySelector("#phone");
+  let phoneData = saveData.phone;
+  inputPhone.value = phoneData;
+  phoneCard.href = phoneData;
+
+  let linkedinCard = document.querySelector(".linkedin"); //linkedin
+  const inputLinkedin = document.querySelector("#linkedin");
+  let linkedinData = saveData.linkedin;
+  inputLinkedin.value = linkedinData;
+  linkedinCard.href = `https://linkedin.com/in/${linkedinData}`;
+
+  let githubCard = document.querySelector(".github"); //github
+  const inputGithub = document.querySelector("#github");
+  let githubData = saveData.github;
+  inputGithub.value = githubData;
+  githubCard.href = `https://github.com/${githubData}`;
+
+  const boxCard = document.querySelector("#box"); //palettes
+  const palettesValue = [
+    "box__card",
+    "box__card--red",
+    "box__card--grey",
+    "box__card--purple",
+    "box__card--orange",
+  ];
+
+  let valuePal = saveData.palette - 1;
+  let classPal = palettesValue[valuePal];
+
+  function removeClassPal() {
+    for (let i = 0; i < palettesValue.length; i++) {
+      boxCard.classList.remove(palettesValue[i]);
+    }
+  }
+  removeClassPal();
+  boxCard.classList.add("box__card");
+  boxCard.classList.add(classPal);
+
+
+};
+
+
+window.addEventListener("load", printLocalStorage);
