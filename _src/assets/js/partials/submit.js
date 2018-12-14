98fbdc6fee21320593e74ec6ff62c11eb7135862
arranzizquierdo <laura.arranz88@gmail.com>
2018-12-14 12:06:58 +0100

    Add text loading

--- _src/assets/js/partials/submit.js
+++ _src/assets/js/partials/submit.js
@@ -1,13 +1,17 @@
 const submitEl = document.querySelector('.button-create');
 function handlerSubmit() {
+    const loadingEl = document.querySelector('.loading');
+    loadingEl.innerHTML ='Estamos creando tu tarjeta...';
     fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
         method: 'POST',
         body: JSON.stringify(dataBack),
         headers: {
             'content-type': 'application/json'
         },
-    }).then((responLink) => responLink.json())
+    })
+    .then((responLink) => responLink.json())
     .then((link) => {
+        loadingEl.innerHTML ="";
         let urlTwitter = link.cardURL;
         const urlTextEl = document.querySelector('.link-profile-card');
         urlTextEl.innerHTML = urlTwitter;
