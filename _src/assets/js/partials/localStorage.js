a95c4089d021d3221cdf5fea40ec538ac3392140
arranzizquierdo <laura.arranz88@gmail.com>
2018-12-14 20:15:40 +0100

    Review conflicts

--- _src/assets/js/partials/localStorage.js
+++ _src/assets/js/partials/localStorage.js
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-
-function saveDates (){
-    localStorage.setItem("dataBackLocal",JSON.stringify(dataBack));
-}
-
-function printLocalStorage(){
-    const saveData = JSON.parse(localStorage.getItem("dataBackLocal"));
-    if (saveData) {
-        dataBack = saveData;
-    }
-
-    let nameCard = document.querySelector(".userInfo__name"); //name
-    const inputName = document.querySelector("#name");
-    let nameData = saveData.name;
-    inputName.value = nameData;
-    nameCard.innerHTML = nameData;
-
-    let jobCard = document.querySelector(".userInfo__job"); //job
-    const inputJob = document.querySelector("#job");
-    let jobData = saveData.job;
-    inputJob.value = jobData;
-    jobCard.innerHTML = jobData;
-
-    let emailCard = document.querySelector(".email"); //mail
-    const inputEmail = document.querySelector("#email");
-    let emailData = saveData.email;
-    inputEmail.value = emailData;
-    emailCard.href = `mailto:${emailData}`;
-
-    let phoneCard = document.querySelector(".mobile"); //phone
-    const inputPhone = document.querySelector("#phone");
-    let phoneData = saveData.phone;
-    inputPhone.value = phoneData;
-    phoneCard.href = phoneData;
-
-    let linkedinCard = document.querySelector(".linkedin"); //linkedin
-    const inputLinkedin = document.querySelector("#linkedin");
-    let linkedinData = saveData.linkedin;
-    inputLinkedin.value = linkedinData;
-    linkedinCard.href = `https://linkedin.com/in/${linkedinData}`;
-
-    let githubCard = document.querySelector(".github"); //github
-    const inputGithub = document.querySelector("#github");
-    let githubData = saveData.github;
-    inputGithub.value = githubData;
-    githubCard.href = `https://github.com/${githubData}`;
-    console.log(githubData);
-=======
 let saveData = JSON.parse(localStorage.getItem("dataBackLocal")); //saveData es mi cadena ahora objeto
 
 function printLocalStorage() {
@@ -72,9 +23,6 @@ function printLocalStorage() {
   let imageData = saveData.photo;
   profileImgLs.style.backgroundImage = `url(${imageData})`;
   profileImgSmallLs.style.backgroundImage = `url(${imageData})`;
-
-
-
 
   let emailCard = document.querySelector(".email"); //mail
   const inputEmail = document.querySelector("#email");
@@ -145,12 +93,7 @@ removeClassTyp();
 
 boxFont.classList.add("userInfo");
     boxFont.classList.add(classTyp);
->>>>>>> json
 };
 
 
 window.addEventListener("load", printLocalStorage);
-<<<<<<< HEAD
-
-=======
->>>>>>> json
