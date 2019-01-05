9328d40958248534b93f479641c1214a96c61868
aroatdlt <aroatato@gmx.com>
2019-01-05 12:09:36 +0100

    Refactoring the code with global functions

--- _src/assets/js/partials/hidden.js
+++ _src/assets/js/partials/hidden.js
@@ -8,46 +8,52 @@ const arrowDesignElement = document.querySelector('.arrowD');
 const arrowFillElement = document.querySelector('.arrowF');
 const arrowShareElement = document.querySelector('.arrowSa');
 
-function handlerHidden(event) {
-  const currentClicked = event.currentTarget; 
-  if (currentClicked === buttonDesignElement){   
-
-    designElement.classList.toggle('hidden');
-    
-    if(arrowDesignElement.classList.contains('fa-chevron-up')){
-      arrowDesignElement.classList.remove('fa-chevron-up');
-      arrowDesignElement.classList.add('fa-chevron-down');
-    }else{
-      arrowDesignElement.classList.remove('fa-chevron-down');
-      arrowDesignElement.classList.add('fa-chevron-up');
-    }
-    fillElement.classList.add('hidden');
-    shareElement.classList.add('hidden');
-
-  }else if (currentClicked === buttonFillElement){
-    hiddenBlockElement (fillElement, shareElement, designElement, arrowFillElement);
-    // fillElement.classList.toggle('hidden');
-    // arrowFillElement.classList.toggle('fa-chevron-up');
-    // shareElement.classList.add('hidden');
-    // designElement.classList.add('hidden');
-
+const currentElementFunction = (currentElement, currentArrow) => {
+  currentElement.classList.toggle('hidden');
+  if (currentElement.classList.contains('hidden')) {
+    currentArrow.classList.add('fa-chevron-down');
+    currentArrow.classList.remove('fa-chevron-up');
   } else {
-    hiddenBlockElement (shareElement, designElement,fillElement, arrowShareElement);
-    // shareElement.classList.toggle('hidden');
-    // arrowShareElement.classList.toggle('fa-chevron-up');
-    // designElement.classList.add('hidden');
-    // fillElement.classList.add('hidden');
+    currentArrow.classList.remove('fa-chevron-down');
+    currentArrow.classList.add('fa-chevron-up');
   }
-}
+};
 
-function hiddenBlockElement (clickedBlock, firstHiddenBlock, secondHiddenBlock, arrow){
-  clickedBlock.classList.toggle('hidden');
-  firstHiddenBlock.classList.add('hidden');
-  secondHiddenBlock.classList.add('hidden');
-  arrow.classList.toggle('fa-chevron-up');
+const firstHiddenElementFunction = (firstHiddenElement, firstArrowElement) => {
+  firstHiddenElement.classList.add('hidden');
+  if (firstArrowElement.classList.contains('.fa-chevron-up')) {
+    firstArrowElement.classList.remove('fa-chevron-up');
+    firstArrowElement.classList.add('fa-chevron-down');
+  }
+};
+
+const secondHiddenElementFunction = (secondHiddenElement, secondArrowElement) => {
+  secondHiddenElement.classList.add('hidden');
+  if (secondArrowElement.classList.contains('.fa-chevron-up')) {
+    secondArrowElement.classList.remove('fa-chevron-up');
+    secondArrowElement.classList.add('fa-chevron-down');
+  }
+};
+
+function handlerHidden(event) {
+  const currentClicked = event.currentTarget;
+  if (currentClicked === buttonDesignElement) {
+    currentElementFunction(designElement, arrowDesignElement);
+    firstHiddenElementFunction(fillElement, arrowFillElement);
+    secondHiddenElementFunction(shareElement, arrowShareElement);
+  }
+  if (currentClicked === buttonFillElement) {
+    currentElementFunction(fillElement, arrowFillElement);
+    firstHiddenElementFunction(designElement, arrowDesignElement);
+    secondHiddenElementFunction(shareElement, arrowShareElement);
+  }
+  if (currentClicked === buttonShareElement) {
+    currentElementFunction(shareElement, arrowShareElement);
+    firstHiddenElementFunction(designElement, arrowDesignElement);
+    secondHiddenElementFunction(fillElement, arrowFillElement);
+  }
 }
 
 buttonDesignElement.addEventListener('click', handlerHidden);
 buttonFillElement.addEventListener('click', handlerHidden);
 buttonShareElement.addEventListener('click', handlerHidden);
-
